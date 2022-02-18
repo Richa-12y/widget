634ed8e819c29b56dc838cb8b5c58a5d5cab5221
Richa-12y <kricha000@gmail.com>
2022-02-18 21:55:21 +0530

    Throutting API Request, Reminder on setTimeout, useEffect's cleaneup Funcation,Implementing  a Delayed Request,Searching on Intial Render

--- src/component/Search.js
+++ src/component/Search.js
@@ -3,11 +3,60 @@ import axios from "axios";
 
 const Search = () => {
 
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('Richa Singh');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
+    const [results, setResults] = useState([]);
 
-    useEffect(async () => {
-        await axios('hgfhfhfhfh')
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
     }, [term]);
+
+    console.log(results);
+
+    useEffect(() => {
+
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: debouncedTerm,
+                }
+            });
+            setResults(data.query.search);
+        };
+        search();
+    }, [debouncedTerm]);
+
+
+
+    const renderedResults = results.map((results) => {
+        return (
+            <div key={results.pageid} className="item">
+                <div className="right floated content">
+                    <a
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${results.pageid}`}
+                    >
+                        Go
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {results.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: results.snippet }}></span>
+                </div>
+            </div>)
+    });
 
     return (
         <div>
@@ -19,6 +68,9 @@ const Search = () => {
                         onChange={event => setTerm(event.target.value)}
                         className="input" />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     )
